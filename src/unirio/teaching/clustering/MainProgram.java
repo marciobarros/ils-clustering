aacf077effe63ffc80566c4026c5531843b5e3f8
Marcio Barros <marcio.barros@gmail.com>
2021-07-08 16:34:33 -0300

    Adaptacao para apresentar o numero de clustes

--- src/unirio/teaching/clustering/MainProgram.java
+++ src/unirio/teaching/clustering/MainProgram.java
@@ -2,24 +2,26 @@ package unirio.teaching.clustering;
 
 import java.io.File;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import unirio.teaching.clustering.model.Project;
 import unirio.teaching.clustering.reader.DependencyReader;
 import unirio.teaching.clustering.search.IteratedLocalSearch;
 import unirio.teaching.clustering.search.constructive.ConstrutiveAbstract;
-import unirio.teaching.clustering.search.constructive.ConstrutiveRandom;
+import unirio.teaching.clustering.search.constructive.ConstrutiveAglomerativeMQ;
 
 public class MainProgram
 {
-	private static String BASE_DIRECTORY = "C:\\Users\\User\\Desktop\\Codigos\\ILS\\data\\clustering\\";
+	private static String BASE_DIRECTORY = "C:\\Users\\User\\Desktop\\Codigos\\ils-clustering\\data\\clustering\\";
 	
 	public static final void main(String[] args) throws Exception
 	{
 		File file = new File(BASE_DIRECTORY + "dependencies");
 		DecimalFormat df4 = new DecimalFormat("0.0000");
 		
-		//ConstrutiveAbstract constructor = new ConstrutiveAglomerativeMQ();
-		ConstrutiveAbstract constructor = new ConstrutiveRandom();
+		ConstrutiveAbstract constructor = new ConstrutiveAglomerativeMQ();
+		//ConstrutiveAbstract constructor = new ConstrutiveRandom();
 
         for (String projectName : file.list()) 
         {
@@ -29,15 +31,30 @@ public class MainProgram
     		Project project = reader.load(BASE_DIRECTORY + "dependencies\\" + projectName);
 
     		IteratedLocalSearch ils = new IteratedLocalSearch(constructor, project, 100_000);
-    		ils.execute();
+    		int[] solution = ils.execute();
     		
     		long finishTimestamp = System.currentTimeMillis();
     		long seconds = (finishTimestamp - startTimestamp);
     		
     		long memory = Runtime.getRuntime().freeMemory() / (1024 * 1024);
-    		System.out.println(padLeft(projectName, 20) + " " + padRight("" + project.getClassCount(), 10) + " " + padRight(df4.format(ils.getBestFitness()), 10) + " " + padRight("" + seconds, 10) + " ms " + padRight("" + memory, 10) + " MB");
+    		System.out.println(padLeft(projectName, 20) + " " + padRight("" + project.getClassCount(), 10) + padRight("" + countClusters(solution), 10) + " " + padRight(df4.format(ils.getBestFitness()), 10) + " " + padRight("" + seconds, 10) + " ms " + padRight("" + memory, 10) + " MB");
     		//System.out.println(projectName + ";" + project.getClassCount() + ";" + df4.format(ils.getBestFitness()));
         }
+	}
+
+	private static int countClusters(int[] solution)
+	{
+		List<Integer> clusters = new ArrayList<Integer>();
+		
+		for (int i = 0; i < solution.length; i++)
+		{
+			int cluster = solution[i];
+			
+			if (!clusters.contains(cluster))
+				clusters.add(cluster);
+		}
+		
+		return clusters.size();
 	}
 
 	public static String padLeft(String s, int length) 
